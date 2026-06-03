4a3e9254d9d0707d5b0187bf745075eddcac6df6
JSD <jsdthe1st@gmail.com>
2026-06-03 22:04:15 +0200

    change tree plotting

--- ClassificationTree.R
+++ ClassificationTree.R
@@ -1,3 +1,64 @@
+draw_tree_plot <- function(tree_res, name, file_name) {
+    png(paste("Plots/tree_plots/", file_name, ".png", sep=""), width = 800, height = 600, res = 80)
+    
+    par(
+        bg = "white", 
+        mar = c(3, 3, 5, 3),
+        cex.main = 1.6,
+        font.main = 2
+    )
+    plot(tree_res, type = "uniform", col = "transparent") 
+    u <- par("usr")
+    rect(u[1], u[3], u[2], u[4], col = "#EBEBEB", border = NA)
+    grid(nx = NULL, ny = NULL, col = "white", lty = 1, lwd = 1.5)
+    par(new = TRUE)
+    plot(tree_res, type = "uniform", col = "#2166AC", lwd = 4)
+    text(
+        tree_res,
+        cex = 0.95,
+        col = "black",
+        font = 2,
+        pretty = 0,
+        bg = "white"
+    )
+    par(mar = c(4, 4, 6, 4))
+    title(main = name, cex.main = 1.8)
+    
+    dev.off()
+}
+
+draw_tree_cv_plot <- function(tree_res.cv, tree_res) {
+    
+    df <- data.frame(
+        size = tree_res.cv$size,
+        deviance = tree_res.cv$dev
+    )
+    
+    p <- ggplot(df, aes(x = size, y = deviance)) +
+        geom_line(color = "#2166AC", linewidth = 1) +
+        geom_point(color = "#2166AC", size = 3) +
+        ggtitle("Błąd CV dla różnych rozmiarów drzewa") +
+        labs(
+            x = "Tree size",
+            y = "Deviance"
+        ) +
+        theme(
+            plot.title = element_text(size = 20, face = "bold", hjust = 0.5),
+            axis.title = element_text(size = 18),
+            axis.text = element_text(size = 18)
+        )
+    
+    print(tree_res)
+    
+    ggsave(
+        "Plots/tree_plots/tree_cv.png",
+        plot = p,
+        width = 8.5,
+        height = 6,
+        dpi = 80
+    )
+}
+
 set.seed(23)
 
 if (!exists("do_preliminary_analisys")) {
@@ -31,16 +92,9 @@ for (i in 2:5) {
   tree_res.cv$dev <- tree_res.cv$dev + cv.tree(tree_res2, FUN = prune.tree)$dev
 }
 tree_res.cv$dev <- tree_res.cv$dev / 5
-png("tree_cv.png", width = 800, height = 600)
-plot(tree_res.cv)
-print(tree_res)
-dev.off()
+draw_tree_cv_plot(tree_res.cv, tree_res)
 
-
-png("tree.png", width = 800, height = 600)
-plot(tree_res)
-text(tree_res)
-dev.off()
+draw_tree_plot(tree_res, "Drzewo klasyfikacyjne", "classification_tree")
 
 summary(tree_res)
 
@@ -54,10 +108,7 @@ library(caret)
 confusionMatrix(predict_test, test$blueWins)
 
 small_tree <- prune.tree(tree_res2, best = 2)
-png("small_tree.png", width = 800, height = 600)
-plot(small_tree)
-text(small_tree)
-dev.off()
+draw_tree_plot(small_tree, "Przycięte drzewo klasyfikacyjne", "trimmed_classification_tree")
 
 # 2. Robimy prognozy dla obu modeli na zbiorze testowym
 pred_small <- predict(small_tree, newdata = test, type = "class")
