0e7a205d295f8e6691f7b5dc8e649f2ae89165bf
Tomasz Żołądek <s230998@sggw.edu.pl>
2026-06-10 22:54:33 +0200

    Add begging of new LaTeX table generator

---
+++ latex/sythetic_table_maker_4000.py
@@ -0,0 +1,32 @@
+import os
+from ctypes import c_wchar
+
+import pandas as pd
+
+repo_path = r"../SyntheticRecords/"
+
+csv_files = os.listdir(repo_path)
+
+string = r'''
+\begin{table}[H]
+  \centering
+\begin{tabular}{l *{5}{c}}
+    \toprule
+    Seria Syntetycznych danych & 1 & 2 & 3 & 4 & 5 \\
+    \midrule
+'''
+
+for file in csv_files:
+    file_path = repo_path + "\\" + file
+
+    print(file_path)
+
+    with open(file_path, "r", encoding="utf-8") as f:
+        file_content = f.read()
+
+
+string += '''
+\end{tabular}
+\caption{5 danych syntetycznych i wyniki predykcji dla nich.}
+\end{table}
+'''
