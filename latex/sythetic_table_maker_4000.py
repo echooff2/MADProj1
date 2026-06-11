38da1c32c58855c77a0ddb01e76999e95681aa94
JSD <jsdthe1st@gmail.com>
2026-06-11 22:56:11 +0200

    finish synth table maker

--- latex/sythetic_table_maker_4000.py
+++ latex/sythetic_table_maker_4000.py
@@ -1,32 +1,44 @@
 import os
-from ctypes import c_wchar
-
+import re
 import pandas as pd
 
-repo_path = r"../csv/"
+folder_path = "..\\csv\\"  # sam se zemin te ukosniki >:(
 
-csv_files = os.listdir(repo_path)
+class_file_names = os.listdir(folder_path)
+feature_file_name = "synth_data_notscaled.csv"
+class_file_names.remove(feature_file_name)
 
-string = r'''
+latex = r'''
 \begin{table}[H]
   \centering
 \begin{tabular}{l *{5}{c}}
     \toprule
     Seria Syntetycznych danych & 1 & 2 & 3 & 4 & 5 \\
-    \midrule
-'''
+    \midrule'''
 
-for file in csv_files:
-    file_path = repo_path + "\\" + file
+feature_file_path = folder_path + feature_file_name
+df = pd.read_csv(feature_file_path)
 
-    print(file_path)
+df = df.drop(columns=df.columns[0])  # drop index column
+df = df[["blueWins"] + [c for c in df.columns if c != "blueWins"]]  # move blueWins to the beginning
+for class_file_name in class_file_names:  # connect dataframes
+    class_file_path = folder_path + class_file_name
+    class_df = pd.read_csv(class_file_path)
+    pattern = r"synth_data_(\w*?).csv"
+    class_name = re.findall(pattern, class_file_name)[0]
+    df.insert(0, class_name, class_df["x"])
 
-    with open(file_path, "r", encoding="utf-8") as f:
-        file_content = f.read()
+for col_name in df:
+    latex_row = "\n"
+    latex_row += "\t" + col_name
+    for value in df[col_name]:
+        latex_row += " & " + str(value)
+    latex += latex_row
 
-
-string += '''
+latex += r'''
 \end{tabular}
 \caption{5 danych syntetycznych i wyniki predykcji dla nich.}
 \end{table}
 '''
+
+print(latex)
